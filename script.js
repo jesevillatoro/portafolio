ba4eae14d6e5967ddfdf7820169a28dc87c59ac2
jesevillatoro <jese.villatoro@gmail.com>
2025-04-14 13:36:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Manejo del formulario de contacto
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para enviar el formulario por AJAX o simplemente mostrar un mensaje.
+    alert('¡Gracias por contactar!');
+  });
+  
